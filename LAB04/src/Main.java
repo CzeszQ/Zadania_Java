ddac3d1fc0396be7ad9c68d7f310e159dfd00224
CzeszQ <162746678+CzeszQ@users.noreply.github.com>
2024-03-17 22:12:19 +0100

    Add files via upload

---
+++ LAB04/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        //Z1
+        //Count.calculate_sum();
+        //Count.calculate_average();
+        /*
+        //Z2
+        int[] tab1 = new int[]{1, 2, 3, 6};
+        Arrays.print_even_index_array(tab1);
+        int[] tab2 = new int[]{1, 2, 3, 6,7};
+        Arrays.print_even_index_array(tab2);
+         */
+        //Z3
+        //Arrays.print_array_apper();
+        /* Z4
+         String[] words = new String[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Podaj słowo " + (i + 1) + ": ");
+            words[i] = Input.input_string;
+        }
+
+        for (int i = 4; i >= 0; i--) {
+            String reversedWord = reverseString(words[i]);
+            System.out.println(reversedWord);
+        }
+    }
+         */
+        //Z5
+        // Sort.sort_array();
+        //z6
+        // Factorial.calculateFactorial();
+        //z7
+        //CompareArrays.comapre();
+    }
+}
+
